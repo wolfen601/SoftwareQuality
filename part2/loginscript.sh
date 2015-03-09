538c78b65a5005ac9620ed979d9df1cea7629b47
Kevin Ho <kevin-ho@live.ca>
2015-03-09 16:16:58 -0400

    Added folder for results and scripts for login and logout

---
+++ part2/loginscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./a.out < ../TestCases/Login/login1.inp.txt > ../TestOutput/Login/login1.out.txt
+./a.out < ../TestCases/Login/login2.inp.txt > ../TestOutput/Login/login2.out.txt
+./a.out < ../TestCases/Login/login3.inp.txt > ../TestOutput/Login/login3.out.txt
+./a.out < ../TestCases/Login/login4.inp.txt > ../TestOutput/Login/login4.out.txt
+./a.out < ../TestCases/Login/login5.inp.txt > ../TestOutput/Login/login5.out.txt
+./a.out < ../TestCases/Login/login6.inp.txt > ../TestOutput/Login/login6.out.txt
+./a.out < ../TestCases/Login/login7.inp.txt > ../TestOutput/Login/login7.out.txt
---
+++ part2/logoutscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./a.out < ../TestCases/Logout/logout1.inp.txt > ../TestOutput/Logout/logout1.out.txt
+./a.out < ../TestCases/Logout/logout2.inp.txt > ../TestOutput/Logout/logout2.out.txt
+./a.out < ../TestCases/Logout/logout3.inp.txt > ../TestOutput/Logout/logout3.out.txt
+./a.out < ../TestCases/Logout/logout4.inp.txt > ../TestOutput/Logout/logout4.out.txt
+./a.out < ../TestCases/Logout/logout5.inp.txt > ../TestOutput/Logout/logout5.out.txt
