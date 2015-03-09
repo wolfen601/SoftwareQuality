cfefcbf92294beb30c4ae62210f12b5a8de22129
Kevin Ho <kevin-ho@live.ca>
2015-03-09 19:21:56 -0400

    added another test case to buy

--- part3/buyscript.sh
+++ part3/buyscript.sh
@@ -5,3 +5,4 @@
 ./a.out < ../TestCases/Buy/buy3.inp.txt > ../TestOutput/Buy/buy3.out.txt
 ./a.out < ../TestCases/Buy/buy4.inp.txt > ../TestOutput/Buy/buy4.out.txt
 ./a.out < ../TestCases/Buy/buy5.inp.txt > ../TestOutput/Buy/buy5.out.txt
+./a.out < ../TestCases/Buy/buy6.inp.txt > ../TestOutput/Buy/buy6.out.txt
